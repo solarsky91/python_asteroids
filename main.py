aed521594131deb8db6f1afd926719393e8f9ee8
jantrobius <jantrobius@deloitte.com>
2025-04-15 09:19:27 -0600

    Pausing due to errors with OS, 2

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
-
 import pygame
 from constants import *
 
+
 def main():
-    # initialize pygame
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    #print("Starting Asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    # game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
         screen.fill("black")
         pygame.display.flip()
-        clock.tick(60)
 
-
-
-
-
-
-
-
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
