0e3414ca3334374027fdeb359e27e80696565acd
jantrobius <jantrobius@deloitte.com>
2025-04-15 09:06:03 -0600

    Initial start of Asteroids!

---
+++ main.py
@@ -0,0 +1,33 @@
+
+import pygame
+from constants import *
+
+def main():
+    # initialize pygame
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        pygame.Surface.fill(screen, (0,0,0))
+        pygame.display.flip()
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
